a6d745a1f8b83d546aed7f1fec0ebe437e05ff37
An7ar35 <eadavison@protonmail.com>
2018-03-15 19:57:07 +0100

    [main] Added media link list info print out + sorted out default -l as a flag.

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
 import argparse
 import sys
 
+from colorama import Fore, Style
+
 import explorer
 from flags import PrefixType
 from writer import TextWriter
 
 
 def main(argv):
+    list_file_name = 'media-list.txt'
+
     parser = argparse.ArgumentParser(prog='mediascrub', description='Download files from a website')
     parser.add_argument('URL', help='Root URL for searching')
     parser.add_argument('-d', '--depth', default=0, type=int, help='Exploration link depth (default: %(default)s)')
     parser.add_argument('-e', '--ext-filter', metavar='N', nargs='+', default=['png', 'jpg', 'gif'],
                         help='Media extension filter (default: %(default)s)')
-    parser.add_argument('-l', '--list', default=False, type=bool,
+    parser.add_argument('-l', '--list', action='store_true',
                         help='Create a list file (media-list.txt) with all media urls (default: %(default)s)')
     parser.add_argument('-m', '--media-filter', metavar='N', nargs='+',
                         help='Media URL case-sensitive filter (default: %(default)s)')
@@ -24,6 +28,7 @@ def main(argv):
                         help='URL case-sensitive filter (default: %(default)s)')
 
     args = parser.parse_args(argv)
+    print("Print media links to file .....: ", Fore.GREEN if args.list else Fore.RED, list_file_name if args.list else 'None', Style.RESET_ALL)
     # print(args)
     scrubber = explorer.LinkExplorer(args.ext_filter,
                                      args.url_filter,
@@ -35,7 +40,7 @@ def main(argv):
 
     if args.list is True:
         writer = TextWriter()
-        writer.open('media-list.txt')
+        writer.open(list_file_name)
         for url in urls:
             writer.writeLine(url)
 
